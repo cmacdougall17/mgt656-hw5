a0c506d67e5888d6881a03bd6423b9de6b341e35
Colin MacDougall <colin.macdougall@yale.edu>
2016-10-07 00:46:39 +0000

    3 commit 3 quit

--- server.js
+++ server.js
@@ -1,8 +1,19 @@
 var express = require('express');
 var app = express();
 
+app.get('', function(request, response){
+    response.send('Hello MGT-656!');
+});
+
 app.get('/', function(request, response){
     response.send('Hello MGT-656!');
 });
 
-app.listen(process.env.PORT || 4000);
+app.get('/nickname', function(request, response){
+    response.send('Hello cloudy-mouse!');
+});
+
+app.listen(
+    process.env.PORT || 4000,
+    process.env.IP || 'local host'
+);
